022e949fd776253fbe7f9145ca7bd5bf2231a420
oumaimabenrabeh96 <98836665+oumaimabenrabeh96@users.noreply.github.com>
2023-01-05 16:33:29 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 # Set up variables
 APP_NAME='ecommerce'
 BUILD_DIR='dist/ecommerce'
-SERVER='example.com'
-REMOTE_DIR='/var/www/$APP_NAME'
+SERVER='http://gamerpc.epizy.com/'
+REMOTE_DIR='/home/vol17_2/epizy.com/epiz_33325654'
 
 # Sync built files to server
 rsync -avzhe ssh --delete $BUILD_DIR/ $SERVER:$REMOTE_DIR
